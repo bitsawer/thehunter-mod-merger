339d10416d68d2e70e9e990fe7fbbe8eea683991
bitsawer <sawerduster@gmail.com>
2022-07-09 23:14:28 +0300

    Improve build script

--- build_windows.py
+++ build_windows.py
@@ -1,20 +1,31 @@
 
 import os
 import shutil
+import struct
 
 from src import version
+
+BITS = struct.calcsize("P") * 8
 
 if os.path.exists("dist"):
     shutil.rmtree("dist")
 
 os.system("pyinstaller src/modmerger.py")
 
+if True:
+    #A bit of a hack: unzip the "base_library.zip" into an actual directory called "base_library.zip".
+    #This is because some file services etc. will reject .zip files containing other .zip-files...
+    #The Python sys.path import machinery should still work just the same.
+    os.system("python -m zipfile -e dist/modmerger/base_library.zip dist/modmerger/base_library_temp")
+    os.remove("dist/modmerger/base_library.zip")
+    os.rename("dist/modmerger/base_library_temp", "dist/modmerger/base_library.zip")
+
 shutil.copyfile("src/original.gdcc", "dist/modmerger/original.gdcc")
 shutil.copyfile("README.md", "dist/README.md")
 os.mkdir("dist/mods")
 os.mkdir("dist/output")
 
-shutil.make_archive("thehunter-mod-merger-%s-win64" % version.VERSION, "zip", root_dir="dist")
+shutil.make_archive("thehunter-mod-merger-win%i-%s" % (BITS, version.VERSION), "zip", root_dir="dist")
 
 shutil.rmtree("dist")
 shutil.rmtree("build")
